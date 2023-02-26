3636b1ce066413d3e0688d2ca1ad3ace0a2cbf63
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-26 16:38:51 -0300

    Configuração de inicialização da api

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+
+
+
+
+app.listen(9090, () => { 
+    console.log('Server is running on port 9090');
+});
