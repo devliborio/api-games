236877cfe4a75402611065144617ff46d46b10c8
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-27 09:43:49 -0300

    Criando banco de dados falso para testar API

--- index.js
+++ index.js
@@ -7,8 +7,33 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Banco de dados falso
+var DB = { 
+    games: [
 
+        {
+            id: 21,
+            title: 'Bayonetta ',
+            year: 2009,
+            price: 36
+        },
 
+        {
+            id: 32,
+            title: 'Subnautica',
+            year: 2014,
+            price: 57
+        },
+
+        {
+            id: 45,
+            title: 'Hogwarts Legacy',
+            year: 2023,
+            price: 250
+        },
+
+    ]
+}
 
 
 
