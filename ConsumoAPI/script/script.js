38f51d6b70503ed517dc0ff8c71de963806836bd
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-03-01 02:15:53 -0300

    organizando código, movendo script para pasta especifica

---
+++ ConsumoAPI/script/script.js
@@ -0,0 +1,43 @@
+(function(){
+
+    function createGame() {
+        let titleInput = document.getElementById("title");
+        let priceInput = document.getElementById("price");
+        let yearInput = document.getElementById("year");
+    
+        let game = {
+            title: titleInput.value,
+            price: priceInput.value,
+            year: yearInput.value
+        }
+        axios.post("http://localhost:9090/game", game).then((response) => {
+    
+            if (response.status == 201) {
+                alert("Game cadastrado!")
+            }
+    
+        }).catch((erro) => {
+            console.log(erro);
+    
+        })
+    }
+    
+    axios.get("http://localhost:9090/games").then((response) => {
+    
+        let games = response.data;
+        let list = document.getElementById("games");
+    
+        games.forEach((game) => {
+    
+            let item = document.createElement("li");
+            item.classList.add("list-group-item");
+            item.innerHTML = game.id + " - " + game.title + " - R$ " + game.price;
+            list.appendChild(item);
+    
+        });
+    
+    }).catch((error) => {
+        console.log(error);
+    })
+
+})()
